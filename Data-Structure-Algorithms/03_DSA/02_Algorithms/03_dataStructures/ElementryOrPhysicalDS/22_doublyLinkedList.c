7994000c774c90f4cee5920726ef651e52ab9266
Pramod Kumar <pramod.kumar20100310@gmail.com>
2025-12-11 20:25:12 +0100

    circu;ar added

--- Data-Structure-Algorithms/03_DSA/02_Algorithms/03_dataStructures/ElementryOrPhysicalDS/22_doublyLinkedList.c
+++ Data-Structure-Algorithms/03_DSA/02_Algorithms/03_dataStructures/ElementryOrPhysicalDS/22_doublyLinkedList.c
@@ -7,7 +7,104 @@ struct node{
     struct node *n;//next
 };
 
+void display(struct node *q){
+    while(q!=NULL){
+        printf("%d ",q->data);
+        q=q->n;
+    }
+    printf("\n");
+}
+
+void append(struct node **s, int data){
+    struct node *r, *q=*s;
+    if(*s==NULL){// if empty
+        *s=(struct node *)malloc(sizeof(struct node));
+        (*s)->p=NULL;
+        (*s)->data=data;
+        (*s)->n=NULL;
+    }else{
+        while (q->n!=NULL){
+            q=q->n;
+        }
+        r=(struct node *)malloc(sizeof(struct node));
+        r->data=data;
+        r->n=NULL;
+        r->p=q;
+        q->n=r;
+    }
+}
+
+void addBegin(struct node **s, int data){
+    struct node *q;
+    q=(struct node *)malloc(sizeof(struct node));
+    q->p=NULL;
+    q->data=data;
+    q->n=*s;
+    (*s)->p=q;
+    *s=q;
+}
+
+void addAtLoc(struct node *s, int loc, int data){
+    struct node *temp;
+    int a;
+    for(a=0;a<loc;a++){
+        s=s->n;
+        if(s==NULL){
+            printf("There are less than %d elements in list\n", loc);
+            return;
+        }
+    }
+
+    s=s->p;
+    temp=(struct node *)malloc(sizeof(struct node));
+    temp->data=data;
+    temp->p=s;
+    temp->n=s->n;
+    temp->n->p=temp;
+    s->n=temp;
+}
+
+void del(struct node **s, int data){
+    struct node *q=*s;
+    while(q!=NULL){
+        if(q->data==data){ // is node found
+            if(q==*s){//if first node to be deleted
+                *s=(*s)->n;
+                (*s)->p=NULL;
+            }else{
+                if(q->n==NULL){ //if last node deleted
+                    q->p->n=NULL;
+                }else{ // if node deleted node in between
+                    q->p->n=q->n;
+                    q->n->p=q->p;
+                }
+                free(q);
+            }
+            return;
+        }
+        q=q->n; //go to next node in list
+    }
+    printf("%d not found in list\n", data);
+}
+
 int main(){
 
+    struct node *q;
+    q=NULL;
+
+    append(&q,12);
+    append(&q,12);
+    display(q);
+    
+    addBegin(&q,122);
+    display(q);
+
+    addAtLoc(q,2,23); // list, location and data
+    addAtLoc(q,3,3);
+    display(q);
+
+    del(&q,12);
+    del(&q,2);
+    display(q);
 
 }
